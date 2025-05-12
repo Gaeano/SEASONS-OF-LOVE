65212693d49a2772c00098da921dac9dc21a882c
Gaeano <francesannagaeamutia@gmail.com>
2025-05-12 22:45:56 +0800

    changed the sequence of line 6

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
     $dbname = "s19010108_seasonsoflove";
     $user = "s19010108_seasonsoflove";
     $password = "mutiamanlangitherediasanding2025";
-    $conn = new mysqli($host, $dbname, $user, $password);
+    $conn = new mysqli($host, $user, $password, $dbname);
     
     if($conn->connect_error){
         die("Connection failed".$conn->connect_error);
