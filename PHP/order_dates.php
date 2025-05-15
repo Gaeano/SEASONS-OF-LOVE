e73d7c61b7ca5cca73e482a056b61c18fc33cea8
LYEMAN <lukasmanlangit@gmail.com>
2025-05-16 02:09:17 +0800

    Polished functionality

--- PHP/order_dates.php
+++ PHP/order_dates.php
@@ -1,10 +1,10 @@
 <?php
 header("Content-Type: application/json");
 
-require __DIR__ . '/db.php'; 
+require __DIR__ . '/db.php';
 
-$sql = "SELECT DISTINCT DATE(order_date) as order_day FROM orders";
+$sql = "SELECT DISTINCT DATE(order_date) as date, status FROM orders";
 $stmt = $pdo->query($sql);
-$dates = $stmt->fetchAll(PDO::FETCH_COLUMN);
+$dates = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 echo json_encode($dates);
