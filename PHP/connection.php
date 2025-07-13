4065cc7229a5c06e03745ebb2cce1114a2852369
Gaeano <francesannagaeamutia@gmail.com>
2025-07-13 18:48:30 +0800

    yeryeyr

--- PHP/connection.php
+++ PHP/connection.php
@@ -1,15 +1,28 @@
 <?php 
-    $host = "localhost";
-    $dbname = "s19010108_seasonsoflove";
-    $user = "s19010108_seasonsoflove";
-    $password = "mutiamanlangitherediasanding2025";
-    $conn = new mysqli($host, $user, $password, $dbname);
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db_name = "db";
+    $conn = new mysqli($servername, $username, $password, $db_name, 3306);
     
     if($conn->connect_error){
         die("Connection failed".$conn->connect_error);
     }
     echo "";
     return $conn;
+
+
+    // $host = "localhost";
+    // $dbname = "s19010108_seasonsoflove";
+    // $user = "s19010108_seasonsoflove";
+    // $password = "mutiamanlangitherediasanding2025";
+    // $conn = new mysqli($host, $user, $password, $dbname);
+    
+    // if($conn->connect_error){
+    //     die("Connection failed".$conn->connect_error);
+    // }
+    // echo "";
+    // return $conn;
 
 ?>
 
