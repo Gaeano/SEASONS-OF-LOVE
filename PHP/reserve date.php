32c0ff3c18cf08772ba73bfe3a65d398cdaca840
LYEMAN <lukasmanlangit@gmail.com>
2025-07-17 17:22:19 +0800

    HELP ME GOD

--- PHP/reserve date.php
+++ PHP/reserve date.php
@@ -31,7 +31,7 @@ while ($row = mysqli_fetch_assoc($result)) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-  <link rel="stylesheet" href="reserve date.css">
+  <link rel="stylesheet" href="../CSS/reserve date.css">
 
   <!--For font for brand-->
   <link rel="preconnect" href="https://fonts.googleapis.com">
