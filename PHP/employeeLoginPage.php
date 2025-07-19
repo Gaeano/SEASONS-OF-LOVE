f6658523c834e18cafa6f77461391aae97914eab
Gaeano <francesannagaeamutia@gmail.com>
2025-07-19 23:32:31 +0800

    yeryer

--- PHP/employeeLoginPage.php
+++ PHP/employeeLoginPage.php
@@ -42,7 +42,7 @@ if (isset($_POST['submit'])) {
                 header("Location: ../HTML/employeePage.php");
                 exit;
             } else {
-                header("Location: ../HTML/reserve date.html");
+                header("Location: ../HTML/reserve date.php");
                 exit;
             }
         }
